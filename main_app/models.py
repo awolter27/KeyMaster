85f2178db7902a5392b8baf2b170142de6dbc0d1
awolter27 <awolter@butler.edu>
2023-07-10 19:10:00 -0400

    Added build.sh, deps.txt, debugging issue with Bootstrap/main.css

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    date_of_birth = models.IntegerField
+    email = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    verified_record = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "User:" + self.first_name + self.last_name
+
+    class Meta:
+        ordering = ["last_name"]
+
+
+class Records(models.Model):
+    login_url = models.CharField(max_length=500)
+    email = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    security_key = models.CharField(max_length=500)
+    security_questions = models.CharField(max_length=1000)
+    notes = models.CharField(max_length=5000)
+
+    def __str__(self):
+        return "Records:" + self.login_url
+
+    class Meta:
+        ordering = ["login_url"]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import redirect
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.shortcuts import render
 from django.views.generic.base import TemplateView
-from django.views.generic import DetailView
-from django.views import View
-from .models import Record
+
+# from django.views.generic.edit import CreateView, UpdateView, DeleteView
+# from django.views.generic import DetailView
+# from django.views import View
+# from .models import Record
 
 
 class Home(TemplateView):
@@ -14,33 +15,33 @@ class SignUp(TemplateView):
     template_name = "sign_up.html"
 
 
-class SignIn(TemplateView):
-    template_name = "sign_in.html"
+# class SignIn(TemplateView):
+#     template_name = "sign_in.html"
 
 
-class RecordsList(TemplateView):
-    template_name = "records_list.html"
+# class RecordsList(TemplateView):
+#     template_name = "records_list.html"
 
 
-class RecordCreate(CreateView):
-    model = Record
-    fields = []
-    template_name = "record_create.html"
-    success_url = "/records/"
+# class RecordCreate(CreateView):
+#     model = Record
+#     fields = []
+#     template_name = "record_create.html"
+#     success_url = "/records/"
 
 
-class Record(DetailView):
-    template_name = "record_detail.html"
+# class Record(DetailView):
+#     template_name = "record_detail.html"
 
 
-class RecordUpdate(UpdateView):
-    model = Record
-    fields = []
-    template_name = "record_update.html"
-    success_url = "/records/"
+# class RecordUpdate(UpdateView):
+#     model = Record
+#     fields = []
+#     template_name = "record_update.html"
+#     success_url = "/records/"
 
 
-class RecordDelete(DeleteView):
-    model = Record
-    template_name = "record_delete.html"
-    success_url = "/records/"
+# class RecordDelete(DeleteView):
+#     model = Record
+#     template_name = "record_delete.html"
+#     success_url = "/records/"
