2e95377a03fa2ae5d717ce684abefe26a6138bff
awolter27 <awolter@butler.edu>
2023-07-09 21:58:33 -0400

    Templates initialized, base.html updated with starter code

--- KeyMaster/urls.py
+++ KeyMaster/urls.py
@@ -20,6 +20,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.stie.urls),
+    path("admin/", admin.site.urls),
     path("", include("main_app.urls")),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import redirect
+from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.base import TemplateView
-from django.views.generic.base import DetailView
+from django.views.generic import DetailView
 from django.views import View
 from .models import Record
 
