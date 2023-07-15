1da262dc163ed1c08f2f9d14bbe483cae2438ce0
awolter27 <awolter@butler.edu>
2023-07-15 12:04:08 -0400

    Authorization complete

--- main_app/models.py
+++ main_app/models.py
@@ -1,24 +1,9 @@
 from django.db import models
-
-
-class User(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    date_of_birth = models.IntegerField
-    email = models.CharField(max_length=50)
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-    verified_user = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return "User:" + self.first_name + self.last_name
-
-    class Meta:
-        ordering = ["last_name"]
+from django.contrib.auth.models import User
 
 
 class Record(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     website = models.CharField(max_length=50)
     login_url = models.CharField(max_length=500)
     email = models.CharField(max_length=50)
