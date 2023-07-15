82793f4493eae76aaae9196941d48e7ba02a6c27
awolter27 <awolter@butler.edu>
2023-07-15 17:11:00 -0400

    Formatting code, drying code

--- main_app/static/scripts/main.js
+++ main_app/static/scripts/main.js
@@ -1,29 +1,27 @@
 const passwordInput = document.querySelector('#passwordInput');
-const passwordGenerate = document.querySelector('.passwordGenerate');
-const passwordToggle = document.querySelector('.passwordToggle');
+const passwordGenerate = document.querySelector('#passwordGenerate');
+const passwordToggle = document.querySelector('#passwordToggle');
 
 const handleGenerate = (e) => {
-    let passwordLength = 20;
     let passwordCharacters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()';
+    let passwordLength = 20;
     let passwordGenerated = '';
-
-    for (let i = 0; i < passwordLength; i++){
-        let passwordRandom = Math.floor(Math.random() * passwordCharacters.length);
-        passwordGenerated += passwordCharacters.charAt(passwordRandom);
+    for (let i = 0; i < passwordLength; i++) {
+        let passwordRandChar = Math.floor(Math.random() * passwordCharacters.length);
+        passwordGenerated += passwordCharacters.charAt(passwordRandChar);
     }
     passwordInput.value = passwordGenerated;
 }
 
 const handleToggle = (e) => {
     if (passwordToggle.textContent === 'SHOW') {
-        passwordToggle.textContent='HIDE';
+        passwordToggle.textContent = 'HIDE';
         passwordInput.setAttribute('type', 'text');
     } else {
-        passwordToggle.textContent='SHOW';
+        passwordToggle.textContent = 'SHOW';
         passwordInput.setAttribute('type', 'password');
     }
 }
 
 passwordGenerate.addEventListener('click', handleGenerate);
 passwordToggle.addEventListener('click', handleToggle);
-
